ec3fe749bc5845e5e71b8ab2b4fd75cd82d904e4
spacecoffin <reed.rosenberg@gmail.com>
2014-10-22 13:07:25 -0400

    As submitted
    booooo

--- concordance.c
+++ concordance.c
@@ -15,7 +15,8 @@
 #define DEBUG 1
 
 /* Function prototypes */
-//int insert(char*, char*, int);
+int insert(char*, char*, int);
+int bin_srch(char*, char**, int);
 //void print(char *, int);
 //void empty(char *, int);
 
@@ -31,17 +32,19 @@ int main(int argc, const char *argv[])
 	int table_size = atoi(argv[1]);	// Set table_size equal to value of 
 					// first command-line arg, converting 
 					// the string to int.
+
+	printf("%i\n", table_size);
 	
-	char Table[table_size];		// The word table should be implemented
+	char word_tbl[table_size][STRMAX];
+	char *table[table_size];	// The word table should be implemented
 					// as an array of pointers to strings.
 
 	char *line = NULL;
 	size_t length = 0;
 	ssize_t read;
-	char word[STRMAX];
-	char *word_ptr;
+	char *word;
 
-	int i = 0;
+	int i = 0, n = 0;
 
 	
 
@@ -50,67 +53,106 @@ int main(int argc, const char *argv[])
 		i++;
 		printf("%i. %s", i, line);
 
-		word_ptr = strtok(line, delimiters);
+		word = strtok(line, delimiters);
 
-		
-
-		while (word_ptr != NULL) {
-			printf("%s\n", word_ptr);
-			//insert()
-			word_ptr = strtok(NULL, delimiters);
+		while ((word != NULL) && (n <= table_size)) {
+			printf("%s\n", word);
+			n = insert(&word, &table, n);
+			word = strtok(NULL, delimiters);
 		}
 	}
 	free(line);
-
-	/*
-	char word[STRMAX];
-	char *word_ptr;
-
-	word_ptr = strtok(word, delimiters);
-
-	while (word_ptr != NULL) {
-		printf("%s\n", word_ptr);
-		//insert()
-		word_ptr = strtok(NULL, delimiters);
-	}
-	
-	//insert();
-	*/
 	
 	return 0;
 }
 
-void print(char *Table[], int n)
+void print(char *table[], int n)
 {
 	// Prints Table in its entirety, which should be in sorted order.
 }
 
-void empty(char *Table[], int n)
+void empty(char *table[], int n)
 {
 	/*
-	 * emptys Table by freeing all the strings currently stored in it and
+	 * emptys table by freeing all the strings currently stored in it and
 	 * setting the string pointers back to NULL.
 	 */
 }
 
-int insert(char *word, char *Table[], int n)
+int insert(char *word, char *table, int n)
 {
         /*
-	 * Inserts the string word into Table, which should be maintained in
+	 * Inserts the string word into table, which should be maintained in
 	 * sorted order. Returns n+1 if the string is successfully inserted;
 	 * otherwise returns n.
 	 */
 	
 	/*
 	 * The function insert() should first determine whether the given word 
-	 * is already in Table. If it is, then the word should not be inserted 
+	 * is already in table. If it is, then the word should not be inserted 
 	 * and the function should simply return the current value of n. On the 
 	 * other hand, if word is not in the table, then it should be inserted 
 	 * into its proper position so that after the insertion is performed, 
-	 * Table maintains its sorted order. The function should then return 
+	 * table maintains its sorted order. The function should then return 
 	 * n+1, the new number of words in the table.
 	 */
-	int m=1;
+
+	char temp[STRMAX];
+
+	if (!(bin_srch(*word, *table, n))) {
+		if (n == 0) {
+			for (i = 0; isalpha(*word[i]); i++) {
+				temp[i] = tolower(*word[i]);
+			}
+			*table[0] = strndup(*temp, STRMAX);
+
+			strdup;
+			n++;
+			printf("Stored %s at beginning", *word)
+		} else if ((n % 2) == 1) {
+
+	}
+	} 
+
+
 	
-	return m;
+	
+	//printf("insert: %s\n", *word);
+	
+	return n;
+}
+
+int bin_srch(char *word, char **table, int n)
+{
+	// Binary search to determine if word is in table.
+	// returns an int expressing if the target value was found in the table
+	// where: 0 = no, 1 = yes. This will be used in insert() to determine
+	// if the target word should be inserted into the table.
+
+	int result;
+
+	if (n == 0) {		// if table is empty
+		return 0;	// then it doesn't contain the target
+	} else if (n == 1) {	// if the table consists of one element
+		result = strncasecmp(*word, **table[0]); // case-insensitive comp.
+		result = (result != 0);
+		return result;
+	} else {
+		int lo = 0, hi = n-1, mid;
+		while (lo <= hi) {
+			mid = lo + (hi-lo)/2;
+			result = strncasecmp(*word, **table[mid]);
+			if (result == 0) {
+				return 1;	// target found in table
+			} else if (result < 0) {
+				lo = mid + 1;
+				continue;
+			} else if (result > 0) {
+				hi = mid - 1;
+				continue;
+			} else {
+				return 0;	// target not in table
+			}
+		}
+	}
 }
